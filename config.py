87ba7b84a3d1fe3d08f123ec3d9cd0c05720970c
yangwei <wei88.yang@tcl.com>
2026-07-09 17:44:20 +0800

    修改管理员后台

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 
-# 是否开启debug模式
-DEBUG = True
 
-# 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+DEBUG = os.environ.get("DEBUG", "false").lower() in {"1", "true", "yes", "y"}
+
+USE_LLM = os.environ.get("USE_LLM", "false")
+SILICONFLOW_API_KEY = os.environ.get("SILICONFLOW_API_KEY", "")
+SILICONFLOW_BASE_URL = os.environ.get("SILICONFLOW_BASE_URL", "https://api.siliconflow.cn/v1")
+SILICONFLOW_MODEL = os.environ.get("SILICONFLOW_MODEL", "Qwen/Qwen3.5-397B-A17B")
+
+RECOMMEND_CACHE_ENABLED = os.environ.get("RECOMMEND_CACHE_ENABLED", "true")
+RECOMMEND_CACHE_PATH = os.environ.get(
+    "RECOMMEND_CACHE_PATH",
+    "/app/wxcloudrun/data/recommendation_cache.json",
+)
