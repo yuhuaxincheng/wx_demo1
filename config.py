9f920a0216af86034960b2b963f8ddef89154836
yuhuaxincheng <158007488+yuhuaxincheng@users.noreply.github.com>
2026-07-09 18:25:32 +0800

    Update RECOMMEND_CACHE_ENABLED default value
    Change default value of RECOMMEND_CACHE_ENABLED to false

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SILICONFLOW_API_KEY = os.environ.get("SILICONFLOW_API_KEY", "Bearer sk-qkmvspzkq
 SILICONFLOW_BASE_URL = os.environ.get("SILICONFLOW_BASE_URL", "https://api.siliconflow.cn/v1")
 SILICONFLOW_MODEL = os.environ.get("SILICONFLOW_MODEL", "Qwen/Qwen3.5-397B-A17B")
 
-RECOMMEND_CACHE_ENABLED = os.environ.get("RECOMMEND_CACHE_ENABLED", "true")
+RECOMMEND_CACHE_ENABLED = os.environ.get("RECOMMEND_CACHE_ENABLED", "false")
 RECOMMEND_CACHE_PATH = os.environ.get(
     "RECOMMEND_CACHE_PATH",
     "/app/wxcloudrun/data/recommendation_cache.json",
